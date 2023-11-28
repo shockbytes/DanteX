9098f8b506c31dfd5a4ab085b63ca3a5fe716ae4
Martin Macheiner <martin@viabirds.com>
2023-11-28 19:51:51 +0100

    Add wishlist page

--- lib/src/ui/wishlist/wishlist_page.dart
+++ lib/src/ui/wishlist/wishlist_page.dart
@@ -1,3 +1,6 @@
+import 'package:dantex/src/data/book/entity/book_state.dart';
+import 'package:dantex/src/ui/core/themed_app_bar.dart';
+import 'package:dantex/src/ui/main/book_state_page.dart';
 import 'package:flutter/material.dart';
 
 class WishlistPage extends StatelessWidget {
@@ -5,9 +8,13 @@ class WishlistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text('TODO Implement Wishlist'),
+    return const Scaffold(
+      // TODO Show app bar only on mobile devices
+      appBar: ThemedAppBar(
+        title: Text('Wishlist'),
+      ),
+      body: Center(
+        child: BookStatePage(BookState.wishlist),
       ),
     );
   }
