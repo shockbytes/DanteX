d63562e80f3553af9861fee9bd317d31935f0485
Lockie Richter <lockie.richter@happy.co>
2024-11-16 14:33:13 +1030

    User provider test

---
+++ test/providers/auth_test.dart
@@ -0,0 +1,46 @@
+import 'package:dantex/data/auth/user.dart';
+import 'package:dantex/providers/auth.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'util.dart';
+
+void main() {
+  group('Given a userProvider', () {
+    group('When the user is null', () {
+      test('Then the provider returns null', () async {
+        final mockFirebaseAuth = MockFirebaseAuth();
+        final container = createContainer(
+          overrides: [
+            firebaseAuthProvider.overrideWithValue(mockFirebaseAuth),
+          ],
+        );
+
+        final subscription = container.listen(userProvider.future, (_, __) {});
+        final user = await subscription.read();
+
+        expect(user, isNull);
+      });
+    });
+    group('When there is a user', () {
+      test('Then the provider returns the user transformed to a DanteUser',
+          () async {
+        final mockFirebaseAuth = MockFirebaseAuth();
+        final container = createContainer(
+          overrides: [
+            firebaseAuthProvider.overrideWithValue(mockFirebaseAuth),
+          ],
+        );
+
+        final subscription = container.listen(userProvider.future, (_, __) {});
+        await container.read(firebaseAuthProvider).signInAnonymously();
+        await container.pump();
+        await container.pump();
+        final user = await subscription.read();
+
+        expect(user, isNotNull);
+        expect(user, isA<DanteUser>());
+      });
+    });
+  });
+}
