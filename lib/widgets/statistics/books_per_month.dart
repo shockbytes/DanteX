3a4f778d9741ef45d0f6f97c8082a084c507f75f
Lockie Richter <lockie.richter@happy.co>
2024-12-16 22:41:41 +1030

    Make goal widget generic

--- lib/widgets/statistics/books_per_month.dart
+++ lib/widgets/statistics/books_per_month.dart
@@ -3,10 +3,10 @@ import 'package:dantex/providers/settings.dart';
 import 'package:dantex/providers/statistics.dart';
 import 'package:dantex/util/date_time.dart';
 import 'package:dantex/widgets/statistics/dante_line_chart.dart';
+import 'package:dantex/widgets/statistics/goal_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lottie/lottie.dart';
 
 class BooksPerMonth extends ConsumerWidget {
   const BooksPerMonth({super.key});
@@ -100,46 +100,12 @@ class _BooksPerMonthGoalDialogState
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('statistics.books_per_month.books_goal').tr(),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Slider(
-            value: _sliderValue,
-            max: 30,
-            min: 1,
-            divisions: 30,
-            label: _sliderValue.toInt().toString(),
-            onChanged: (value) {
-              setState(() {
-                _sliderValue = value;
-                _animationController.value = _mapSliderToLottieProgress(value);
-              });
-            },
-          ),
-          const Text('statistics.books_per_month.books_month_count')
-              .tr(args: [_sliderValue.toInt().toString()]),
-          ClipRect(
-            child: Align(
-              alignment: Alignment.topCenter,
-              heightFactor: 0.8,
-              child: Lottie.asset(
-                'assets/lottie/reading-goal.json',
-                controller: _animationController,
-                onLoaded: (composition) {
-                  // Set the controller's duration to match the animation duration
-                  _animationController
-                    ..duration = composition.duration
-                    // Set the initial progress
-                    ..value = _mapSliderToLottieProgress(_sliderValue);
-                },
-              ),
-            ),
-          ),
-          Text(
-            getProficiencyLabel(),
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ],
+      content: GoalWidget(
+        initialValue: widget.initialValue,
+        maxValue: 30,
+        minValue: 1,
+        divisions: 30,
+        labelKey: 'statistics.books_per_month.books_month_count',
       ),
       actions: [
         TextButton(
@@ -168,31 +134,5 @@ class _BooksPerMonthGoalDialogState
         ),
       ],
     );
-  }
-
-  /// Maps the slider values (1-30) to Lottie progress
-  double _mapSliderToLottieProgress(double sliderValue) {
-    const lottieMin = 0.25;
-    const lottieMax = 0.33;
-    const sliderMin = 1;
-    const sliderMax = 30;
-
-    // Map the slider value to the Lottie animation progress range
-    return lottieMin +
-        (sliderValue - sliderMin) *
-            (lottieMax - lottieMin) /
-            (sliderMax - sliderMin);
-  }
-
-  String getProficiencyLabel() {
-    if (_sliderValue / 29 <= 0.25) {
-      return 'reading_goal.rookie'.tr();
-    } else if (_sliderValue / 29 <= 0.5) {
-      return 'reading_goal.serious_reader'.tr();
-    } else if (_sliderValue / 29 <= 0.75) {
-      return 'reading_goal.reading_brain'.tr();
-    } else {
-      return 'reading_goal.book_wizard'.tr();
-    }
   }
 }
---
+++ lib/widgets/statistics/goal_widget.dart
@@ -0,0 +1,111 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class GoalWidget extends StatefulWidget {
+  const GoalWidget({
+    required this.maxValue,
+    required this.minValue,
+    required this.labelKey,
+    required this.divisions,
+    super.key,
+    this.initialValue,
+  });
+
+  final int? initialValue;
+  final int maxValue;
+  final int minValue;
+  final int divisions;
+  final String labelKey;
+
+  @override
+  State<GoalWidget> createState() => _GoalWidgetState();
+}
+
+class _GoalWidgetState extends State<GoalWidget>
+    with SingleTickerProviderStateMixin {
+  late double _sliderValue;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _sliderValue =
+        widget.initialValue?.toDouble() ?? widget.minValue.toDouble();
+    _animationController = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Slider(
+          value: _sliderValue,
+          max: widget.maxValue.toDouble(),
+          min: widget.minValue.toDouble(),
+          divisions: widget.divisions,
+          label: _sliderValue.toInt().toString(),
+          onChanged: (value) {
+            setState(() {
+              _sliderValue = value;
+              _animationController.value = _mapSliderToLottieProgress(value);
+            });
+          },
+        ),
+        Text(widget.labelKey).tr(args: [_sliderValue.toInt().toString()]),
+        ClipRect(
+          child: Align(
+            alignment: Alignment.topCenter,
+            heightFactor: 0.8,
+            child: Lottie.asset(
+              'assets/lottie/reading-goal.json',
+              controller: _animationController,
+              onLoaded: (composition) {
+                // Set the controller's duration to match the animation duration
+                _animationController
+                  ..duration = composition.duration
+                  // Set the initial progress
+                  ..value = _mapSliderToLottieProgress(_sliderValue);
+              },
+            ),
+          ),
+        ),
+        Text(
+          _getProficiencyLabel(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ],
+    );
+  }
+
+  /// Maps the slider values (1-30) to Lottie progress
+  double _mapSliderToLottieProgress(double sliderValue) {
+    const lottieMin = 0.25;
+    const lottieMax = 0.33;
+
+    // Map the slider value to the Lottie animation progress range
+    return lottieMin +
+        (sliderValue - widget.minValue) *
+            (lottieMax - lottieMin) /
+            (widget.maxValue - widget.minValue);
+  }
+
+  String _getProficiencyLabel() {
+    if (_sliderValue / widget.maxValue <= 0.25) {
+      return 'reading_goal.rookie'.tr();
+    } else if (_sliderValue / widget.maxValue <= 0.5) {
+      return 'reading_goal.serious_reader'.tr();
+    } else if (_sliderValue / widget.maxValue <= 0.75) {
+      return 'reading_goal.reading_brain'.tr();
+    } else {
+      return 'reading_goal.book_wizard'.tr();
+    }
+  }
+}
